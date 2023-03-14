21db509a427b11e975ceb2390923327a7b57a27e
Patton <Michael.Patton@Wildlife.ca.gov>
2023-03-14 14:08:22 -0700

    Integrating new data

---
+++ RCode/PR/Effort/PR_Effort.R
@@ -0,0 +1,202 @@
+# PR Observed Effort 2004_2015 ---------------------------------------------
+
+# Michael Patton (michael.patton@wildlife.ca.gov)
+# script that processes the effort i1 table to provide a cleaned up and aggregated effort for each ID
+
+#USER INPUT:
+# copy path to where you downloaded the shared CRFS_Mapping folder between the ()
+working_directory = r"(C:\Users\MPatton\OneDrive - California Department of Fish and Wildlife\CRFS_Mapping)"
+setwd(working_directory)
+
+#load in required packages, if R says the package is not installed then run the following code in the console below install.packages("packagename") so for example install.packages("data.table")
+library(stringi)
+library(tidyverse)
+library(lubridate)
+library(here)
+options(scipen = 999)
+
+# sources the script that is used to clean up the i8 table, returns a single variable 'all_locations' that provides the cleanup blocks at the ID level. Went through a series of filters as well. See other script for more information. 
+source(here('RCode', "PR", "Locations", 'PR_Location.R'))
+
+
+
+# this line is required when sourcing multiple R scripts. The plan is to run all required catch and effort scripts in a "master" script to avoid having to run everything one by one. This line makes sure the required objects are not removed when sourcing multiple scripts. Youll see it in the other scripts as well.
+rm(list = ls()[!ls() %in% c("oc_by_id_agg_04_15", "oe_by_id_agg_04_15", "rc_by_id_agg_04_15", 'all_locations')])
+
+# remove id with location since location is not used with effort. Additionally, there are some situations where they same blocks are used in different locations. This leads to double counting in effort
+all_locations_effort = all_locations %>%
+  select(-id_loc, -HLDEPTH2, -HLDEPTH, -freq, -ddlat, -ddlong) %>%
+  unique()
+
+
+#read in the i1 table 
+oe = fread(file=here("RCode", "PR", "Dat04to15", "Data", "PR_i1_2004-2015_487087r.csv"), fill = T, na.string = c("",".") )  %>%
+  mutate_all(as.character)
+
+new_oe <- fread(file = here('RCode', 'PR', 'Dat16toPresent', 'Data', 'i1_data_16to21.csv'), fill = TRUE) %>%
+  mutate_all(as.character) %>%
+  rename(DAYSF = daysf)
+
+
+#remove all columns that are only NAs
+not_all_na <- function(x) any(!is.na(x))
+oe = oe %>% select(where(not_all_na))
+
+setdiff(names(new_oe), names(oe))
+#[1] "geara"  "boatn"  "assnid" "port"   "daysf"  "gearB"  "island" "nolic"  "missed" "Ref #" 
+
+setdiff(names(oe), names(new_oe))
+
+
+oe = bind_rows(oe, new_oe)
+
+### temporary code to investigate prim1 prim2 issue
+Sp<- fread(here("Lookups", "SpeciesList210510.csv" )) 
+Sp<- Sp %>% 
+  select(PSMFC_Code, Common_Name, TripType_Description) %>% 
+  mutate(PSMFC_Code = as.character(PSMFC_Code)) %>%
+  filter(Common_Name != "bivalve class") %>%
+  filter(!is.na(PSMFC_Code)) %>%
+  mutate(PSMFC_Code = as.character(PSMFC_Code))
+
+#create summary of the different prim1/prim2 combinations
+prims_stat = oe %>%
+  group_by(prim1, prim2) %>% 
+  count() %>%
+  left_join(Sp, by = c("prim1" = "PSMFC_Code")) %>%
+  mutate(prim1 = Common_Name) %>%
+  select(-Common_Name) %>%
+  rename(Trip_1 = TripType_Description) %>%
+  left_join(Sp, by = c("prim2" = "PSMFC_Code")) %>%
+  mutate(prim2 = Common_Name) %>%
+  select(-Common_Name) %>%
+  rename(Trip_2 = TripType_Description) %>%
+  arrange(desc(n)) %>%
+  group_by(prim1, Trip_1, prim2, Trip_2) %>%
+  summarise(n = sum(n)) %>%
+  arrange(desc(n)) %>%
+  mutate(different_type = Trip_1 != Trip_2) %>%
+  mutate(different_type =  ifelse(is.na(different_type), FALSE, different_type))
+
+#write.csv(prims_stat, "Outputs/Observed_Effort_Prim1vPrim2.csv", row.names = FALSE, na = "")
+
+# cleans up id, date, month and year. 
+# add in prim1 vs prim2 logic where prim1 is only used unless prim1 == 'Invertebrates"
+#Selects only relevant columns 
+oe <- oe %>% 
+  mutate(id = as.character(ID_CODE), 
+         date = ymd(stri_sub(ID_CODE, 6, 13)), 
+                    month = month(date), 
+                    year = year(date))%>%
+  select(id, year, month, date, prim1, prim2, CNTRBTRS, DAYSF, HRSF) %>%
+  left_join(Sp %>% select(PSMFC_Code, TripType_Description), by = c("prim1" = "PSMFC_Code")) %>%
+  mutate(primary = ifelse(TripType_Description == "Invertebrates" & !is.na(TripType_Description), prim2, prim1)) %>%
+  select(-TripType_Description)
+
+#clean up DAYSF and CNTRBTRS fields so NAs are 0, if days are reported as 0 in the data but CNTRBTRS were reported, change the days fished to 1 (NEW LOGIC)
+oe <- oe %>% 
+  mutate(DAYSF = as.numeric(ifelse(is.na(DAYSF), 0, DAYSF)), 
+         CNTRBTRS = as.numeric(ifelse(is.na(CNTRBTRS), 0 , CNTRBTRS))) %>%
+  mutate(DAYSF = ifelse(DAYSF == 0 & CNTRBTRS > 0, 1, DAYSF))
+
+
+    #Create species table by extracting data from SpeciesList.csv and merge species data with dfr_angrep data frame 
+Sp<- fread(here("Lookups", "SpeciesList210510.csv" )) 
+Sp<- Sp %>% 
+  select(ALPHA5, PSMFC_Code, Common_Name, TripType_Description) %>% 
+  mutate(PSMFC_Code = as.character(PSMFC_Code)) %>%
+  filter(Common_Name != "bivalve class") %>%
+  filter(!is.na(PSMFC_Code)) %>%
+  rename(alpha = ALPHA5)
+
+
+# pull out data that is not used because it uses a bad sp_code
+notused <- oe %>%
+  anti_join(Sp, by = c("primary" = "PSMFC_Code")) %>%
+  mutate(Reason = "Primary Species not found in species lookup.")
+unique(notused$primary) # looks like a lot of sp codes re reported as the alpha code need to clean this up
+# NFOTH and others are used to indicate a non fishing vessel so are rightfully excluded
+
+byyear = notused %>% group_by(year) %>% count()
+
+# join species lookup to data
+oe_species <- oe %>%
+  inner_join(Sp, by = c("primary" = "PSMFC_Code"))
+
+#summary of the different trip type descriptions found in the data
+type_summary = oe_species %>%
+  group_by(TripType_Description) %>%
+  count() %>%
+  arrange(desc(n)) # some invertebrates still make it through
+
+# summary of the different common names of sought species in the data. 
+common_name_summary = oe_species %>% 
+  group_by(Common_Name) %>%
+  count() %>%
+  arrange(desc(n))
+
+# check to make sure no duplicates were created or rows were lost
+nrow(oe) == (nrow(oe_species) + nrow(notused))
+
+test_id = oe_species %>%
+  group_by(id) %>%
+  count()
+
+oe_species_loc <- oe_species %>%
+  inner_join(all_locations_effort, by = c("id" = "ID_CODE")) %>%
+  rename(year = year.x)
+
+# checks for a bad join where id is duplicated
+test_id2 = oe_species_loc %>%
+  group_by(id, year) %>%
+  count() %>%
+  inner_join(test_id, by = 'id') %>%
+  filter(n.x != n.y)
+
+notused2 <- oe_species %>%
+  anti_join(all_locations_effort, by = c("id"= "ID_CODE")) %>%
+  mutate(Reason = "Does not have corresponding location data by ID")
+
+byyear = notused2 %>% group_by(year) %>% count()
+
+# do NOT normalize effort to the number of blocks visited so no block information is needed to be brought in
+dat <- oe_species_loc %>% 
+  mutate(DaysPerBlock = DAYSF, 
+         CntrbPerBlock = CNTRBTRS, 
+         VesselPerBlock = 1)
+
+# pivot the data so each block reported for a single id has its own row. Counts are already normalized by blocks visited so this will not double count anything but greatly simplifies the logic that WINN uses to generate summary statistics
+by_block = dat %>%
+  pivot_longer(Bk1Bx1a:extrablock11, names_to = "col", values_to = 'Block') %>%
+  filter(!is.na(Block)) %>%
+  select(-col, -total_blocks) %>%
+  unique() # remove situations where the same block is entered into different block columns
+
+# QA check for duplicate combinations that will lead to double counting
+y = by_block %>% 
+  group_by(id, year, Block, TripType_Description) %>% 
+  count() %>%
+  filter(n > 1)
+
+
+
+# aggregate effort to the id-block-species level. This will later be aggregated to the Triptype level but wanted to leave species in for now. 
+oe_by_id_agg = by_block %>%
+  group_by(id, date, month, year, Block, Common_Name, TripType_Description) %>%
+  summarise(Days = sum(DaysPerBlock, na.rm = T), 
+            Cntrbs = sum(CntrbPerBlock, na.rm = T), 
+            Vessels = sum(VesselPerBlock, na.rm = T),
+            test_n = n()) %>%
+  mutate(AnglerDays = Cntrbs*Days) %>%
+  arrange(id) %>%
+  rename(id_noloc = id)
+
+# create summary of data that is not used and the provided reason
+notused_summary = data.frame(c(unique(notused$Reason), unique(notused2$Reason)), 
+                             c(nrow(notused), nrow(notused2)))
+names(notused_summary) = c("Reason", "Count")
+
+
+
+write.csv(oe_by_id_agg, "Outputs/oe.csv", na = "", row.names = F)
+write.csv(notused_summary, "Outputs/oe_notused_summary.csv", na = "", row.names = F)
---
+++ RCode/PR/Rp.Catch/PR_ReportedCatch.R
@@ -0,0 +1,135 @@
+# PR Reported Catch 2004_2015 ---------------------------------------------
+
+# Michael Patton's simplification of original script (PR_ObservedEffort_2004_2015km Part 1 and art2.R)
+#7/28/2022
+
+# copy path to where you downloaded the shared CRFS_Mapping folder between the ()
+working_directory = r"(C:\Users\MPatton\OneDrive - California Department of Fish and Wildlife\CRFS_Mapping)"
+setwd(working_directory)
+
+#load in required packages, if R says the package is not installed then run the following code in the console below: install.packages("packagename") so for example install.packages("data.table")
+library(data.table)
+library(stringi)
+library(tidyverse)
+library(lubridate)
+library(here)
+options(scipen = 999)
+
+# to put the cart before the horse, this line is required when sourcing multiple R scripts. The plan is to run all required catch and effort scripts in a "master" script to avoid having to run everything one by one. This line makes sure the required objects are not removed when sourcing multiple scripts. Youll see it in the other scripts as well.
+rm(list = ls()[!ls() %in% c("oc_by_id_agg_04_15", "oe_by_id_agg_04_15", "rc_by_id_agg_04_15", 'all_locations')])
+
+# sources the script that is used to clean up the i8 table, returns a single variable 'all_locations' that provides the cleanup blocks at the ID level. Went through a series of filters as well. See other script for more information. 
+source(here('RCode', "PR", "Locations", 'PR_Location.R'))
+
+
+# read in the i2 table for reported catch
+rc = fread(here("RCode", "PR", "Dat04to15", "Data", "PR_i2_2004-2015_429673r.csv"), fill = T, na.string = c("",".") )  %>%
+  mutate_all(as.character)
+
+rc_16on <- fread(file = here('RCode', 'PR', 'Dat16toPresent', 'Data', 'i2_data_16to21.csv'), fill = TRUE) %>%
+  mutate_all(as.character)
+
+setdiff(names(rc_16on), names(rc))
+# [1] "assnid"  "Ref #"   "Rels_DD"
+setdiff(names(rc), names(rc_16on))
+
+rc = bind_rows(rc, rc_16on)
+
+# Not used data will be combined as a separate output for review. Reasons are provided in new column. 
+notused = rc %>%
+  filter(is.na(as.numeric(SP_CODE)))  %>%
+  mutate(Reason = "SP_CODE is not valid.")
+unique(notused$SP_CODE)
+
+
+# remove invalid SP CODEs, create new id from ID_CODE and location number (locn), extract date, month and year. Select only needed columns. 
+rc <- rc %>% 
+  mutate(SP_CODE = as.numeric(SP_CODE)) %>%
+  filter(!is.na(SP_CODE)) %>%
+  mutate(locn = ifelse(is.na(locn) | locn == 0, 1, locn),
+         id = paste(ID_CODE, locn, sep= ""), 
+         date = ymd(stri_sub(ID_CODE, 6, 13)), 
+         month = month(date)) %>%  
+  select(id, date, month, year = YEAR, prim1, prim2, SP_CODE, MODE_F, MODE_FX, CNTRBTRS, DISPO, NUM_FISH) 
+
+#Create species table by extracting data from SpeciesList.csv and merge species data with dfr_angrep data frame 
+Sp<- fread(here("Lookups", "SpeciesList210510.csv" )) 
+Sp<- Sp %>% 
+  select(PSMFC_Code, Common_Name, TripType_Description, ALPHA5) %>% 
+  mutate(PSMFC_Code = as.numeric(PSMFC_Code)) %>%
+  filter(Common_Name != "bivalve class") 
+
+notused2 <- rc %>%
+  anti_join(Sp, by = c("SP_CODE" = "PSMFC_Code")) %>%
+  mutate(Reason = "Species not found in Lookup.")
+unique(notused2$SP_CODE)
+
+byyear = notused2 %>% group_by(year) %>% count()
+
+# join catch data with species
+rc_species <- rc %>%
+  inner_join(Sp, by = c("SP_CODE" = "PSMFC_Code")) 
+
+# make sure no duplicates or rows were lost in join
+nrow(rc) == (nrow(rc_species) + nrow(notused2))
+
+# Merge together catch and location data ----------------------------------
+
+rc_species_loc <- rc_species %>%
+  inner_join(all_locations, by = c("id"= "id_loc"))  %>% 
+  select(id, ID_CODE, date, month, year = year.x, SP_CODE, ALPHA5, Common_Name, TripType_Description, prim1, prim2, SP_CODE, MODE_F, CNTRBTRS, DISPO, NUM_FISH, HLDEPTH, HLDEPTH2, ddlat, ddlong, Bk1Bx1a, Bk1Bx1b, Bk1Bx1c, Bk2Bx2a, Bk2Bx2b, Bk2Bx2c, extrablock7,  extrablock8,  extrablock9,  extrablock10, extrablock11, HGSIZE, hgsize2, total_blocks)
+
+notused3 <- rc_species %>%
+  anti_join(all_locations, by = c("id"= "id_loc")) %>%
+  mutate(Reason = "Does not have corresponding location data by ID")
+
+byyear = notused3 %>% group_by(year) %>% count()
+
+dat <- rc_species_loc %>% 
+  mutate(FishPerBlock = as.numeric(NUM_FISH)/total_blocks)
+
+
+unique(dat$DISPO)
+# sort fish in Released or kept by DISPO code
+dat   <- dat %>% 
+  mutate(FishPerBlock = ifelse(!is.na(FishPerBlock), FishPerBlock, 0)) %>%
+  mutate(Rep.Released  = ifelse(DISPO %in% c(1,2,6), FishPerBlock, 0), 
+         Rep.Kept = ifelse(DISPO %in% c(3,4,5,7), FishPerBlock, 0))
+
+# CONTRBTRS field creates duplicate entries for the same datapoint
+duplicates = dat %>%
+  group_by(id, SP_CODE, DISPO) %>%
+  count()
+
+dat = dat %>%
+  left_join(duplicates)
+
+# filter out data that had duplicate fishing counts, we may want to look into salvaging this data since contrbtrs is not important for catch
+notused4 = dat %>%
+  filter(n > 1) %>%
+  mutate(Reason = "Duplicate IDs again")
+
+rc_final = dat %>%
+  filter(n == 1)
+
+# pivot so each block-id has its own row. Makes summaries easier. Already normalized to fish per block so no double counting will occur
+by_block = rc_final %>%
+  pivot_longer(Bk1Bx1a:extrablock11, names_to = "col", values_to = 'Block') %>%
+  filter(!is.na(Block))
+
+rc_by_id_agg = by_block %>%
+  group_by(id, ID_CODE, date, month, year, Block,  SP_CODE, Common_Name) %>%
+  summarise(Rep_Released = sum(Rep.Released, na.rm = T), 
+            Rep_Kept  = sum(Rep.Kept, na.rm = T)) %>%
+  mutate(Total_Rep_Fish_Caught = Rep_Released + Rep_Kept)
+
+# create summary of data that is not used and the provided reason
+notused_summary = data.frame(c(unique(notused$Reason), unique(notused2$Reason), unique(notused3$Reason), unique(notused4$Reason)), 
+                             c(nrow(notused), nrow(notused2), nrow(notused3), nrow(notused4)))
+names(notused_summary) = c("Reason", "Count")
+
+
+
+write.csv(rc_by_id_agg, "Outputs/rc.csv", na = "", row.names = F)
+write.csv(notused_summary, "Outputs/rc_notused_summary.csv", na = "", row.names = F)
+
